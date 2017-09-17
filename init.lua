31950c00260dc671f1693a3fe3bfa8c746b57253
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-09-17 13:36:37 +0100

    initial code import

---
+++ init.lua
@@ -0,0 +1,23 @@
+if _G["modns"] then error("modns should not already be defined") end
+local registered = {}
+local checkpath = function(path)
+	if type(path) ~= "string" then error("component path must be a string") end
+end
+
+modns = {
+	register = function(path, component)
+		checkpath(path)
+		if registered[path] then error("duplicate component registration for "..path) end
+		registered[path] = component
+	end,
+	get = function(path)
+		checkpath(path)
+		exists = registered[path]
+		if not exists then error("component does not exist: "..path) end
+		return exists
+	end,
+	check = function(path)
+		checkpath(path)
+		return (registered[path] ~= nil)
+	end
+}
